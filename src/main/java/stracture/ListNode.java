f60898dc9af313f018976e0896f21f2603f172f5
Administrator <375874825@qq.com>
2020-07-28 22:53:16 +0800

    第二题，未完成

---
+++ src/main/java/stracture/ListNode.java
@@ -0,0 +1,9 @@
+package stracture;
+
+import java.io.Serializable;
+
+public class ListNode implements Serializable {
+    public int val;
+    public ListNode next;
+    public ListNode(int x) { val = x; }
+}
--- src/main/java/utils/PrintUtils.java
+++ src/main/java/utils/PrintUtils.java
@@ -1,10 +1,18 @@
 package utils;
 
+import stracture.ListNode;
+
 public class PrintUtils {
     //TODO 泛型怎么用
     public static void printArray(int[] s){
         for (int i:s){
             System.out.println(i);
+        }
+    }
+    public static void printListNode(ListNode listNode){
+        while (listNode!=null){
+            System.out.println(listNode.val);
+            listNode=listNode.next;
         }
     }
 }
